3b383906f630ab8612b4cd6bfb9692ba39ffbed6
ktrer123 <ktrer123@naver.com>
2022-03-15 21:13:42 +0900

    0314-2 EXTENDS / BLOCK / VAR / MVP.CSS

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("UPload");
 export const deleteVideo = (req, res) => {
